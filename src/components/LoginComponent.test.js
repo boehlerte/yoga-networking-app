fe651353f45838777b421a4dfaea1d0bbb674f7b
Erin Boehlert <eringraceboehlert@gmail.com>
2019-01-13 20:24:50 -0500

    remove duplicate test

--- src/components/LoginComponent.test.js
+++ src/components/LoginComponent.test.js
@@ -7,9 +7,3 @@ it('renders without crashing', () => {
   ReactDOM.render(<LoginComponent />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<LoginComponent />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
